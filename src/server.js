169c65a8ca4df895dd79084a4bf552a2450495c2
Neul <neulseo420@gmail.com>
2021-08-02 23:09:07 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const handleHome = (req, res) => {
     return res.end();
 }
 
-const handleLogin = (req, res, next) => {
+const handleLogin = (req, res, next) => { // middleware
     return res.send("login here.");
 }
 app.get("/", handleHome);
