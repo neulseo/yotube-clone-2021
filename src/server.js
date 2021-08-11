847d817de1d5718fb0016f5e173f3afa6cea9f8c
Neul <neulseo420@gmail.com>
2021-08-11 11:29:41 -0400

    Practice middleware2

--- src/server.js
+++ src/server.js
@@ -17,17 +17,31 @@ const PORT = 4000; // we send our requests to PORT (window name: 4000; conventio
 //----------
 
 // EXAMPLE: #3.5
-const gossipMiddleware = (req, res, next) => {
+const logger = (req, res, next) => {
     console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>")
+    } 
     next();
 }
 
 const handleHome = (req, res) => {
     return res.send("IS THIS A MIDDLEWARE?!");
 };
+
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the Private Lounge.");
+}
 // EXAMPLE: #3.6
-app.use(gossipMiddleware);
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome);
+app.get("/protected", handleProtected);
 //----------
 
 
