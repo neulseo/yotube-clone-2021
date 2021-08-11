d298b4747c40fa79d22705561c7af445c0a5a803
Neul <neulseo420@gmail.com>
2021-08-11 11:20:18 -0400

    Practice middleware

--- src/server.js
+++ src/server.js
@@ -4,33 +4,58 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000; // we send our requests to PORT (window name: 4000; conventional)
 const app = express(); // how to create an Express application 
-app.use(morgan("dev"));
+const PORT = 4000; // we send our requests to PORT (window name: 4000; conventional)
 
-const routerLogger = (req, res, next) => {
-    console.log("PATH", req.path);
+// app.get("/", () => console.log("Let me go home!")); // a function has to be sent
+
+// EXAMPLE: #3.3
+// const handleHome = (req, res) => {
+//     return res.send("<h1>DOOODODO</h1>");
+// }
+// app.get("/", handleHome);
+//----------
+
+// EXAMPLE: #3.5
+const gossipMiddleware = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
     next();
+}
+
+const handleHome = (req, res) => {
+    return res.send("IS THIS A MIDDLEWARE?!");
 };
-
-const methodLogger = (req, res, next) => {
-    console.log("METHOD", req.method);
-    next()
-};
-
-const home = (req, res) => {
-    console.log("Responding in console!");
-    return res.send("Hello, responding!");
-};
+// EXAMPLE: #3.6
+app.use(gossipMiddleware);
+app.get("/", handleHome);
+//----------
 
 
-app.use("/global", globalRouter);
-app.use("/user", userRouter);
-app.use("/videos", videoRouter);
+// app.use(morgan("dev"));
+
+// const routerLogger = (req, res, next) => {
+//     console.log("PATH", req.path);
+//     next();
+// };
+
+// const methodLogger = (req, res, next) => {
+//     console.log("METHOD", req.method);
+//     next()
+// };
+
+// const home = (req, res) => {
+//     console.log("Responding in console!");
+//     return res.send("Hello, responding!");
+// };
+
+
+// app.use("/global", globalRouter);
+// app.use("/user", userRouter);
+// app.use("/videos", videoRouter);
 
 
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}💨`);
+const handleListening = () => console.log(`Server listening on port ${PORT}!:)`);
 
 app.listen(PORT, handleListening); // very common in nodeJS --> fn(config,handler)
 
