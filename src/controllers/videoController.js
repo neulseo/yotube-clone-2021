ec323ca1f8dbd3570cefee50a759b17062496942
Neul <neulseo420@gmail.com>
2021-08-12 10:31:14 -0400

    Update controllers

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,7 @@
 const trending = (req, res) => res.send("Home Page Videos");
 
+const watch = (req, res) => res.send("Watch");
+const edit = (req, res) => res.send("Edit");
+
 export default trending;
 
