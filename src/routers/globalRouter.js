be87fe1590a1e6e193a11eaae8f208d5432eee59
Neul <neulseo420@gmail.com>
2021-08-22 19:02:58 -0400

    Review

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,15 +1,10 @@
 import express from "express";
-import { join } from "../controllers/userController";
-import { trending } from "../controllers/videoController";
 
-const globalRouter = express.Router(); 
+const globalRouter = express.Router();
 
 const handleHome = (req, res) => res.send("Home");
-// const handleJoin = (req, res) => res.send("Join");
 
-// globalRouter.get("/", trending);
-// globalRouter.get("/join", join);
-// globalRouter.get("/login", login);
-// globalRouter.get("/search", search);
+globalRouter.get("/", handleHome);
 
-// export default globalRouter;
+export default globalRouter;
+
