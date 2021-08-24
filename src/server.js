6ef99c3804ffc550c1b4ac3fbae9cba930450edc
Neul <neulseo420@gmail.com>
2021-08-24 07:43:24 -0400

    Review #3.0

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
-// import express from "express";
+import express from "express";
 // import morgan from "morgan";
 // // import globalRouter from "./routers/globalRouter";
 // // import videoRouter from "./routers/videoRouter";
 // // import userRouter from "./routers/userRouter";
 
-// const app = express(); // how to create an Express application 
-// const PORT = 7000; // we send our requests to PORT (window name: 4000; conventional)
+const app = express(); // how to create an Express application 
+const PORT = 4000; // we send our requests to PORT (window name: 4000; conventional)
+const handleListening = () => {
+    console.log(`✅ Server listening on port http://localhost:${PORT}!`);
+}
+
+app.listen(PORT, handleListening); // (req, res)
+
 // const logger = morgan("dev");
 // app.use(logger);
 
 // app.use("/", globalRouter);
 // app.use("/users", userRouter);
 // app.use("/videos", videoRouter);
-
-// const handleListening = () => {
-//     console.log(`✅ Server listening on port http://localhost:${PORT}!`);
-// }
