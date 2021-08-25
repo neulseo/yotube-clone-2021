f017d88e36a0388dbb43b73bbb52a3c45b095b2c
Neul <neulseo420@gmail.com>
2021-08-25 16:04:12 -0400

    Review Middleware

--- src/server.js
+++ src/server.js
@@ -7,10 +7,14 @@ import express from "express";
 const app = express(); // how to create an Express application 
 const PORT = 4000; // we send our requests to PORT (window name: 4000; conventional)
 
-const handleHome = (req, res) => {
-    return res.send("YEEPEE!");
+const handleHome = (req, res, next) => {
+    return res.send("<h1>Love Love Love</h1>");
 };
+const handleLogin = (req, res) => {
+    return res.send( { message: "Login Here." });
+}
 app.get("/", handleHome); // a function must be sent as a res!
+app.get("/login", handleLogin);
 
 const handleListening = () => {
     console.log(`✅ Server listening on port http://localhost:${PORT}!`);
