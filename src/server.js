41477e68c987d50010c3ca789659800a3588957b
Neul <neulseo420@gmail.com>
2021-08-27 12:08:36 -0400

    Review #3.5

--- src/server.js
+++ src/server.js
@@ -7,24 +7,19 @@ import express from "express";
 const app = express(); // how to create an Express application 
 const PORT = 4000; // we send our requests to PORT (window name: 4000; conventional)
 
-const handleHome = (req, res, next) => {
-    return res.send("<h1>Love Love Love</h1>");
-};
-const handleLogin = (req, res) => {
-    return res.send( { message: "Login Here." });
+const gossipMiddleware = (req, res, next) => {
+    // console.log("I'm in the middle!");
+    return res.send("this is the power of middleware!!!");
+    next();
 }
-app.get("/", handleHome); // a function must be sent as a res!
-app.get("/login", handleLogin);
+
+const handleHome = (req, res) => {
+    return res.send("This is in the middle.");
+};
+app.get("/", gossipMiddleware, handleHome);
 
 const handleListening = () => {
     console.log(`✅ Server listening on port http://localhost:${PORT}!`);
 }
 
 app.listen(PORT, handleListening); // (req, res)
-
-// const logger = morgan("dev");
-// app.use(logger);
-
-// app.use("/", globalRouter);
-// app.use("/users", userRouter);
-// app.use("/videos", videoRouter);
