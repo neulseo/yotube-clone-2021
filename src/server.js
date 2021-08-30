10b69c50aba49fc90330ad3c9aea26bc2fa7b8ce
Neul <neulseo420@gmail.com>
2021-08-30 14:09:22 -0400

    Review #3.11

--- src/server.js
+++ src/server.js
@@ -1,38 +1,24 @@
 import express from "express";
-// import morgan from "morgan";
+import morgan from "morgan";
 // // import globalRouter from "./routers/globalRouter";
 // // import videoRouter from "./routers/videoRouter";
 // // import userRouter from "./routers/userRouter";
 
 const app = express(); // how to create an Express application 
 const PORT = 4000; // we send our requests to PORT (window name: 4000; conventional)
+const logger = morgan("dev");
 
-const gossipMiddleware = (req, res, next) => {
-    console.log(`${req.method} to ${req.url}`);
-    next();
-}
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url == "/protected") {
-        return res.send("<h1>Not Allowed.</h1>");
-    }
-    next();
-}
-
-const handleHome = (req, res) => {
-    return res.send("This is in the middle.");
+const home = (req, res) => {
+    return res.send("This is Home.");
 };
 
-const handleProtected = (req, res) => {
-    return res.send("Welcome to the Secret Middleware Zone.");
-}
+const login = (req, res) => {
+    return res.send("login");
+};
 
-app.use(gossipMiddleware); // use first, then get. the order is important!
-app.use(privateMiddleware);
-
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () => {
     console.log(`✅ Server listening on port http://localhost:${PORT}!`);
