d9c71eae8f7490c3aa6808d2369ea81289f0778c
Neul <neulseo420@gmail.com>
2021-09-01 11:21:22 -0400

    Review #4.1

--- src/server.js
+++ src/server.js
@@ -1,27 +1,32 @@
 import express from "express";
 import morgan from "morgan";
-// // import globalRouter from "./routers/globalRouter";
-// // import videoRouter from "./routers/videoRouter";
-// // import userRouter from "./routers/userRouter";
 
 const app = express(); // how to create an Express application 
 const PORT = 4000; // we send our requests to PORT (window name: 4000; conventional)
 const logger = morgan("dev");
-
-const home = (req, res) => {
-    return res.send("This is Home.");
-};
-
-const login = (req, res) => {
-    return res.send("login");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+// Create routers
+const globalRouter = express.Router();
+const userRouter = express.Router();
+const videoRouter = express.Router();
+
+// Application is ready to understand differnt urls
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+// Put functions in each router
+const handleHome = (req, res) => res.send("Home");
+const handleEditUser = (req, res) => res.send("Edit User");
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+globalRouter.get("/", handleHome);
+userRouter.get("/edit", handleEditUser);
+videoRouter.get("/watch", handleWatchVideo);
+
 
 const handleListening = () => {
     console.log(`✅ Server listening on port http://localhost:${PORT}!`);
 }
-
 app.listen(PORT, handleListening); // (req, res)
